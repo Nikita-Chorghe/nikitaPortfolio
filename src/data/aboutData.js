fbba403bfdc798962cb779aaf4f7195316e99232
Nikita Chorghe <63181530+Nikita-Chorghe@users.noreply.github.com>
2023-10-19 14:24:11 -0500

    About section

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Hello World!",
-    description1: "My name is Nikita Chorghe.  I am a Software Developer.",
+    description1: "Hello, I'm Nikita Chorghe, a passionate Software Developer on the hunt for exciting full-time opportunities.",
     description2: "I am currently pursuing Master’s in Computer Science at university of Texas at Dallas. My formal academic training has also provided me with opportunities to work with Python, SQL, React, ExpressJS, NodeJS, AWS. I also have experience to manage code with git and working and developing APIs Working individually or as a part of a team, I successfully completed numerous projects focusing on various concepts of Data structure, Machine Learning, Database Management system, Data warehousing and mining. On a personal level, I am a confident, hard-working, and detail-oriented person, passionate about learning new technologies and challenging myself with new tasks.",
     image: 2
 }
